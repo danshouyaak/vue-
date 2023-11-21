8e1cab6b9734a20866cf45cede08c4ff4dd122fb
移动223廖家宇 <113924712@qq.com>
2023-11-21 09:36:57 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import router from '@/router'
 Vue.config.productionTip = false
 
 new Vue({
+  router,   // 把弄好的路由到这里注册
   render: h => h(App),
 }).$mount('#app')
---
+++ src/pages/Search/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ this.$route.params.keyword }}</div>
+</template>
+  
+  <script>
+export default {
+  name: "Search",
+  data(){
+    return{
+    }
+  },
+  created(){
+    console.log(this.$route.params.keyword);
+  }
+};
+</script>
+  
+  <style>
+</style>
